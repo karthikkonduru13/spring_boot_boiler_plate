97a95c3fc1e664f49632b08338632db6fc3adae2
Karthik <karthik@gmail.com>
2025-10-25 10:21:10 +0530

    working fine - v3

--- src/main/java/com/vendor/demo/controller/CloudAPIService.java
+++ src/main/java/com/vendor/demo/controller/CloudAPIService.java
@@ -43,7 +43,7 @@ public class CloudAPIService {
 
   @GetMapping
   public String checkAPIStatus() {
-      return "the MicroService API is up and running - om sai ram - v2";
+      return "the MicroService API is up and running - om sai ram - v3";
   }
 
 }
